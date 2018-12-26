ab6c02d9becbf962a2c53bfe83bb57d89323540c
轩仲宽 <954955937@qq.com>
2018-12-26 16:37:02 +0800

    first commit

---
+++ richText.vue
@@ -0,0 +1,32 @@
+<template>
+	<import src="./wxParse/wxParse.axml"/>
+	<template is="wxParse" :data="{wxParseData:urlNew.nodes}"/>
+</template>
+<script>
+export default {
+    config: {
+    },
+
+    props: {
+        urlNew: String
+    },
+
+    components: {
+    },
+
+    data: {
+    },
+	created(){
+		var WxParse = require('./wxParse/wxParse.js');
+		var that = this;
+		WxParse.wxParse('urlNew', 'html', that.urlNew, that, 5);
+	},
+    methods: {
+
+    }
+};
+</script>
+<style lang="less">
+@import "./wxParse/wxParse.acss";
+
+</style>
